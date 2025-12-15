29e945b3051d7fa45321f2310e928ec2d6325e04
A7medMg <asilme023@gmail.com>
2025-12-15 19:08:26 +0200

    init project.

---
+++ lib/homeScreen.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+  static const String routeName = 'Home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Home Screen!'),
+      ),
+    );
+  }
+}
